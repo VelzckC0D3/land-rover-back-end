d08f9b7ee5a22c643233838b8ebfe738c7d9fb2e
emmanuel mhango <emmanuellmhango@gmail.com>
2023-08-16 22:33:32 +0200

    Added validations for the models

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,6 @@
 class Car < ApplicationRecord
   has_many :reservations
+
+  validates :name, presence: true
+  validates :price, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :reservations
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
