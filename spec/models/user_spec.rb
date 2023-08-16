9d2bda8cbc8184e5d7b51ad95dd7396c8130af5b
Booze33 <tebetisloh@gmail.com>
2023-08-16 17:12:41 -0500

    Fixing linters

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,25 +3,25 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it 'is not valid without a name' do
     user = User.new(email: 'test@gamil.com', password: 'password')
-  expect(user).not_to be_valid
+    expect(user).not_to be_valid
   end
 
   it 'is not valid without a emial' do
     user = User.new(name: 'test', password: 'password')
-  expect(user).not_to be_valid
+    expect(user).not_to be_valid
   end
 
   it 'is not valid without a password' do
     user = User.new(name: 'test', email: 'test@gmail.com')
-  expect(user).not_to be_valid
+    expect(user).not_to be_valid
   end
 
-  it "is valid with valid attributes" do
+  it 'is valid with valid attributes' do
     user = User.new(name: 'King', email: 'king@gmail.com', password: 'password')
     expect(user).to be_valid
   end
 
-  it "has many reservations" do
+  it 'has many reservations' do
     association = described_class.reflect_on_association(:reservations)
     expect(association.macro).to eq :has_many
   end
