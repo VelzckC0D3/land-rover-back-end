5bf482a28f32fd9bf5d7960d7079cc46b55ece2e
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-08-30 06:12:11 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,46 @@ User.create!( name: "Admin", email: "admin@ruby.com", password: "asd123", role: 
 User.create!( name: "User A", email: "user-a@ruby.com", password: "asd123" )
 User.create!( name: "User B", email: "user-b@ruby.com", password: "asd123" )
 
-Car.create!( name: "Defender", price: 6000, description: "The Defender stands as a captivating model with its contemporary electronic system and additional features.", front_image: "https://i.postimg.cc/tJ1KH4fs/front.webp", semi_front_image: "https://i.postimg.cc/05mJ0Rcf/semi-front.webp", side_image: "https://i.postimg.cc/Fz22FQ5D/side.webp", semi_back_image: "https://i.postimg.cc/wMhyqL7P/semi-back.webp", back_image: "https://i.postimg.cc/PJGvn2rY/back.webp", color: "#ffb400" )
-Car.create!( name: "Discovery", price: 7000, description: "The Discovery captivates as a splendid model featuring a modern electronic system and an array of extras.", front_image: "https://i.postimg.cc/tJ1KH4fs/front.webp", semi_front_image: "https://i.postimg.cc/05mJ0Rcf/semi-front.webp", side_image: "https://i.postimg.cc/Fz22FQ5D/side.webp", semi_back_image: "https://i.postimg.cc/wMhyqL7P/semi-back.webp", back_image: "https://i.postimg.cc/PJGvn2rY/back.webp", color: "#d43b3b" )
-Car.create!( name: "Range Rover", price: 8000, description: "The Range Rover entices as an exquisite model equipped with a contemporary electronic system and a host of enhancements.", front_image: "https://i.postimg.cc/tJ1KH4fs/front.webp", semi_front_image: "https://i.postimg.cc/05mJ0Rcf/semi-front.webp", side_image: "https://i.postimg.cc/Fz22FQ5D/side.webp", semi_back_image: "https://i.postimg.cc/wMhyqL7P/semi-back.webp", back_image: "https://i.postimg.cc/PJGvn2rY/back.webp", color: "#3484d5" )
-Car.create!( name: "Range Rover Velar", price: 9000, description: "The Range Rover Velar shines as a remarkable model with its modern electronic system and a myriad of supplementary features.", front_image: "https://i.postimg.cc/tJ1KH4fs/front.webp", semi_front_image: "https://i.postimg.cc/05mJ0Rcf/semi-front.webp", side_image: "https://i.postimg.cc/Fz22FQ5D/side.webp", semi_back_image: "https://i.postimg.cc/wMhyqL7P/semi-back.webp", back_image: "https://i.postimg.cc/PJGvn2rY/back.webp", color: "#7857d5" )
+Car.create!( name: "Defender",
+    price: 6000,
+    description: "The Defender stands as a captivating model with its contemporary electronic system and additional features.",
+    front_image: "https://i.postimg.cc/tJ1KH4fs/front.webp",
+    semi_front_image: "https://i.postimg.cc/05mJ0Rcf/semi-front.webp",
+    side_image: "https://i.postimg.cc/Fz22FQ5D/side.webp",
+    semi_back_image: "https://i.postimg.cc/wMhyqL7P/semi-back.webp",
+    back_image: "https://i.postimg.cc/8czYSgJc/back.webp",
+    color: "#ffb400"
+)
+
+Car.create!( name: "Discovery",
+    price: 7000,
+    description: "The Discovery captivates as a splendid model featuring a modern electronic system and an array of extras.",
+    front_image: "https://i.postimg.cc/7P54fbxm/front.webp",
+    semi_front_image: "https://i.postimg.cc/d3b405qP/semi-front.webp",
+    side_image: "https://i.postimg.cc/tR8jPyJy/side.webp",
+    semi_back_image: "https://i.postimg.cc/VvrK1xPW/semi-back.webp",
+    back_image: "https://i.postimg.cc/76QKFxdL/back.webp",
+    color: "#d43b3b"
+)
+
+Car.create!( name: "Range Rover Evoque",
+    price: 8000,
+    description: "The Range Rover entices as an exquisite model equipped with a contemporary electronic system and a host of enhancements.",
+    front_image: "https://i.postimg.cc/NMynf91X/front.webp",
+    semi_front_image: "https://i.postimg.cc/vTfSfJ2H/semi-front.webp",
+    side_image: "https://i.postimg.cc/cHVbKxmb/side.webp",
+    semi_back_image: "https://i.postimg.cc/NGHn3XGq/semi-back.webp",
+    back_image: "https://i.postimg.cc/T39FkXdK/back.webp",
+    color: "#3484d5"
+)
+
+Car.create!( name: "Range Rover Velar",
+    price: 9000,
+    description: "The Range Rover Velar shines as a remarkable model with its modern electronic system and a myriad of supplementary features.",
+    front_image: "https://i.postimg.cc/NjsBGDLr/front.webp",
+    semi_front_image: "https://i.postimg.cc/SKj4CK6S/semi-front.webp",
+    side_image: "https://i.postimg.cc/1zTQHZBp/side.webp",
+    semi_back_image: "https://i.postimg.cc/qv10NKCD/semi-back.webp",
+    back_image: "https://i.postimg.cc/d3Gw3S8G/back.webp",
+    color: "#7857d5"
+)
