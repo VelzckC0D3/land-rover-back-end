691904d0eacc106a196148d545c15ddedb33c22d
Booze33 <tebetisloh@gmail.com>
2023-08-31 07:26:31 -0500

    Updated users_controller.rb

--- spec/Request/users_controller_spec.rb
+++ spec/Request/users_controller_spec.rb
@@ -26,5 +26,35 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     end
   end
 
-  
+  describe 'GET #show' do
+    it 'returns a success response with the requested user' do
+      user = User.create(name: 'Test User', email: 'test@example.com', password: 'password')
+      get :show, params: { id: user.id }
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)).to eq(user.as_json)
+    end
+  end
+
+  describe 'POST #create' do
+    it 'creates a new user' do
+      post :create, params: { user: { name: 'New User', email: 'new@example.com', password: 'newpassword', password_confirmation: 'newpassword' } }
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)['success']).to be_truthy
+      expect(JSON.parse(response.body)['user']['name']).to eq('New User')
+    end
+
+    it 'returns unprocessable_entity response on validation error' do
+      post :create, params: { user: { name: '', email: 'invalidemail', password: 'password', password_confirmation: 'password' } }
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+
+  describe 'PATCH #update' do
+    let!(:user) { User.create(name: 'Test User', email: 'test@example.com', password: 'password') }
+
+    it 'returns unprocessable_entity response on validation error' do
+      patch :update, params: { id: user.id, user: { email: 'invalidemail' } }
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
 end
