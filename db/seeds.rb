1545982856953e0b2f74ccc234c660e59345d675
Danfall963@Gmail.com <Danfall963@gmail.com>
2023-08-31 01:56:46 -0400

    Config file for Render Deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
 User.create!( name: "Admin", email: "admin@ruby.com", password: "asd123", role: "admin" )
-
 User.create!( name: "User A", email: "user-a@ruby.com", password: "asd123" )
 User.create!( name: "User B", email: "user-b@ruby.com", password: "asd123" )
 
